7443a15322b208ca6a5035169721b3930800b85d
Nikesh Shah <156276029+Nikesh-Shah@users.noreply.github.com>
2026-03-19 08:44:49 +0545

    update: import new routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <Router>
       <div className="App">
         <Routes>
-          
+          <Route path="/" element={<Home />} />
+
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
         </Routes>
